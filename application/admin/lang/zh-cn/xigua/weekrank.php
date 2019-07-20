a6cc76e595bd6722d0b9aab7715596105eb23a82
ljl <a156124198@126.com>
2019-07-20 14:25:00 +0800

    西瓜周榜

---
+++ application/admin/lang/zh-cn/xigua/weekrank.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'Id'           => 'ID',
+    'Xigua_id'     => '西瓜ID',
+    'Room_id'      => '直播间ID',
+    'Nickname'     => '昵称',
+    'Rank'         => '周排行',
+    'Score'        => '钻石',
+    'Avatar_thumb' => '头像',
+    'Level'        => '等级',
+    'Remark'       => '备注',
+    'State'        => '状态',
+    'State 1'      => '显示',
+    'State 2'      => '隐藏',
+    'Ranktime'     => '榜单时间',
+    'Createtime'   => '创建时间',
+    'Updatetime'   => '更新时间'
+];
