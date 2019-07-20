a896392ca96f16c05db1c9f265549ff8e4debd7f
ljl <a156124198@126.com>
2019-07-20 12:26:02 +0800

    火山周榜

---
+++ application/admin/lang/zh-cn/huoshan/weekrank.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Id'           => 'ID',
+    'Display_id'   => '火山号',
+    'Short_id'     => '火山ID',
+    'Room_id'      => '直播间ID',
+    'Nickname'     => '昵称',
+    'Rank'         => '周排行',
+    'Score'        => '火力',
+    'Avatar_thumb' => '头像',
+    'Level'        => '等级',
+    'Remark'       => '备注',
+    'State'        => '状态',
+    'State 1'      => '显示',
+    'State 2'      => '隐藏',
+    'Ranktime'     => '榜单时间',
+    'Createtime'   => '创建时间',
+    'Updatetime'   => '更新时间'
+];
