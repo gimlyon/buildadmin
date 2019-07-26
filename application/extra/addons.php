a8b903a9a0b4b823d29719dd4271d0b15ff198fa
ljl <a156124198@126.com>
2019-07-26 17:25:57 +0800

    安装多个插件

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,8 +4,34 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'app_init' => 
+    array (
+      0 => 'epay',
+    ),
+    'action_begin' => 
+    array (
+      0 => 'geetest',
+    ),
+    'config_init' => 
+    array (
+      0 => 'geetest',
+    ),
+    'admin_login_init' => 
+    array (
+      0 => 'loginbg',
+    ),
+    'user_sidenav_after' => 
+    array (
+      0 => 'recharge',
+    ),
   ),
   'route' => 
   array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
+    '/qrcode$' => 'qrcode/index/index',
+    '/qrcode/build$' => 'qrcode/index/build',
   ),
 );
